cd36e04db002a0de982b5bbe468dbdbe75d48e62
gouravpannu00-cloud <gouravpannu00@gmail.com>
2026-07-22 15:33:06 +0530

    Add files via upload

---
+++ RANDOM_CASING_NO.py
@@ -0,0 +1,21 @@
+# import random 
+# num = random.randint(1, 100)
+# attempts = 0
+
+# print("===Random casing game ===")
+
+# while True :
+#     guess = int (input("Guess a number between 1 and 100: "))
+#     attempts += 1
+
+#     if guess < num :
+#         print("Too Low !", "Try again")
+
+#     elif  guess > num :
+#         print("Too high ! Try again ! ")
+
+#     else :
+#         print("you win !")
+
+#     print("total attempts : ", attempts)
+#     break
