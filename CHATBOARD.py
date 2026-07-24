77fe924eef0c7cbe2549a21e58ad352cb58e26f5
gouravpannu00-cloud <gouravpannu00@gmail.com>
2026-07-24 12:13:25 +0530

    Add files via upload

---
+++ CHATBOARD.py
@@ -0,0 +1,18 @@
+# def chatbot():
+#     print("Bot: Hello! Type 'bye' to exit.")
+
+#     while True:
+#         user = input("You: ").lower()
+
+#         if user == "hello":
+#             print("Bot: Hi there!")
+#         elif user == "how are you":
+#             print("Bot: I'm doing well. Thanks for asking!")
+#         elif user == "bye":
+#             print("Bot: Goodbye!")
+#             break
+#         else:
+#             print("Bot: I don't understand that.")
+
+# chatbot()
+
