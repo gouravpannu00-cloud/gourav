54aec73994cda18593b933b1303b53cfd0370a9f
gouravpannu00-cloud <gouravpannu00@gmail.com>
2026-07-24 12:11:44 +0530

    Add files via upload

---
+++ PALINDROME.py
@@ -0,0 +1,6 @@
+# n = input("Enter a string to check if it is a palindrome: ")
+# rev = n[::-1]
+# if n == rev :
+#     print("The string is a palindrome.")
+# else :
+#     print("The string is not a palindrome.")
