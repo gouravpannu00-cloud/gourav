cb0d5e3e6db799a1ce586ef14d58e3bafaef2653
gouravpannu00-cloud <gouravpannu00@gmail.com>
2026-07-25 11:59:24 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,16 @@
+# My first Python program
+
+print("Hello, World!")
+
+# Ask the user for their name
+
+name = input("Enter your name: ")
+print("Hello,", name + "!")
+
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
