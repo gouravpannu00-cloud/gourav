3c34dcd222ae9f36b9f94d5a8d749481ad877a36
gouravpannu00-cloud <gouravpannu00@gmail.com>
2026-07-27 16:00:11 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,109 @@
+# i=1
+# while i <=5:
+#     print("hello")
+#     i+=1
+# #print(i)
+ 
+#       #     prime numbers from 1 to 5  
+# i=1
+# while i <=5:
+#     print(i)
+#     i+=1
+
+#     #    print no. from 5 to 1
+# i=5
+# while i >=1:
+#     print(i)
+#     i-=1
+
+#     #   print multiplication  table of a no. n
+# i=1
+# while i<=10:
+#     print(3*i)
+#     i+=1
+
+
+#     #    print elements of following list :
+#     #    (1,4,9,16,25,36,49,64,81,100)
+# i=1
+# while i <=10:
+#     print(i**2)
+#     i+=1
+
+#     #     search for no. X in the touple using loop :
+#     #     (1,4,9,16,25,36,49,64,81,100)
+# nums = (1,4,9,16,25,36,49,64,81,100) 
+# x=36
+# i=0
+# while i <len(nums):
+#     if(nums[i]==x):
+#         print("found at idx",x)
+#     else:
+#         print("finding..")    
+#     i+=1    
+     
+     #  leap and normal year 
+# year=int(input("enter a year:"))
+# if(year%400==0)or(year%4==0 and year % 100 !=0):
+#     print("leap year ")
+# else:
+#     print("normal year")
+
+# list=[1,2,3]
+# for el in list:
+#     print(el)
+
+# list =[1,4,9,16,25,36,49,64,81,100]
+# for el in list :
+#     print(el)
+
+# list2=[1,4,9,16,25,36,49,64,81,100]
+# X=36
+# for el in list2:
+#     if(el==X):
+#         print("number found ",X)
+#    # print("finding..")
+
+
+#        #   RANGE 
+# seq = range(10)
+# for i in seq:
+#     print(i)  
+
+#               #  print numbers from 1 to 100
+# for i in range (1,101):
+#     print(i)
+
+#               # print numbers from 100 to 1  
+# for i in range (100,0,-1):
+#     print(i)
+
+#          #   print the multiplication table of a number   n
+# i = 1
+# while i<=10:
+#     print(3*i)
+#     i+=1
+
+
+              #   print all even numbers between 1 to 20
+# i=2
+# while i<=20:
+#     print(i)
+#     i+=2
+
+            #  print all odd numbers between 1 to 20
+# i=1
+# while i<=20:
+#     print(i)
+#     i+=2
+  
+      #     even     number 
+# for i in range (2,101,2):
+#     print(i)
+             
+
+#             #   odd   number 
+# for j in range (1,100,2):
+#     print(j)
+
+
