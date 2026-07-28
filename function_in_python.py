8ce7bf2cf3e21fea3ac228c2966267563dd0ef80
gouravpannu00-cloud <gouravpannu00@gmail.com>
2026-07-28 21:07:10 +0530

    Add files via upload

---
+++ function_in_python.py
@@ -0,0 +1,18 @@
+def calc_sum(a,b):
+    sum=a+b
+    print(sum)
+calc_sum(10,10)
+calc_sum(2,5)
+
+
+def print_hello():
+    print("hello")
+print_hello()
+
+
+def calc_avg(a,b,c):
+    sum=a+b+c 
+    avg=sum/3
+    print(avg)
+    return avg
+calc_avg(2,4,6)    
