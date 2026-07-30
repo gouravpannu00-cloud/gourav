62743e0c46332860ab5cff1212915c15e18268b0
gouravpannu00-cloud <gouravpannu00@gmail.com>
2026-07-30 18:37:53 +0530

    Add files via upload

---
+++ string_operations.py
@@ -0,0 +1,54 @@
+               ##       indexing   slicing   
+
+# name=("Gourav Pannu")
+# num=[1,2,3,4,5]
+    
+# print(num[3])
+# print(name[4])
+
+# print(num[0:5])
+# print(name[0:6])
+
+# print(num[0:5:2])
+# print(name[0:12:3])
+
+# print(num[:4])
+# print(name[:7])
+
+# print(name[::-1])
+# print(num[::-1])
+
+#          ##    questions
+# num_1=[10,20,30,40,50,60] 
+# print(num_1[0:3])
+
+# text="artificial intelligence"
+# print(text[0:10])
+
+# list=[45,55,66,83,86,65,89]
+# print(list[1:6])
+
+# list[1:5]=[10,20,30,40]
+# print(list)
+
+# data=[[1,2,3],[4,5,6],[7,8,9]]
+# for i in data:
+#     print(i[-2:])
+
+# nums=[10,20,30,40,50,60]
+# print(nums[4:0:-1])
+
+# text="abcdefghijklmnopqrstuvwxyz"
+# print(text[20:5:-1])
+
+# nums2=[5,10,15,20,25,30,35,40,45,50]
+# print(nums2[6:2:-1])
+
+# str="madam"
+# if str==str[::-1]:
+#     print("string is palindrome:",str)
+# else:
+#     ("string is not palindrome:",str)
+
+# data2=[1,2,3,4,5,6,7,8,9,10]
+# print(data2[::-2])
