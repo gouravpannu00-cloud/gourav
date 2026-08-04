d7f03aa06d069c6759c5bfdc6722ddef8a8bedb3
gouravpannu00-cloud <gouravpannu00@gmail.com>
2026-08-04 17:05:34 +0530

    Add files via upload

---
+++ 26_june_TEST.py
@@ -0,0 +1,92 @@
+# name=input("enter your name:")
+# age=input("enter your age:")
+# print("hello:",name )
+
+
+# a=10
+# b=5
+# sum=a+b
+# print(sum)
+# multy=a*b
+# print(multy)
+# sub=a-b
+# print(sub)
+# div=a/b
+# print(div)
+
+# a=5
+# b=10
+# a,b=b,a
+# print(f"a={a},b={b}")
+
+# radius=5
+# area=22/7*(radius**2)
+# print(area)
+
+# number=int(input("enter your number:"))
+# if number % 2 == 0:
+#     print("EVEN")
+# else:
+#     print("ODD")
+
+
+# year=int(input("enter your year:"))
+# if (year % 4 == 0 and year%100 ==0)or(year % 400 == 0):
+#     print("this is a leap year")
+# else:
+#     print("this is a normal year")
+
+# marks=int(input("enter your marks:"))
+# if marks>=90 :
+#     print ("Grade A")
+# else:
+#     marks>=80
+#     print("Grade B")
+
+# i=1
+# while i <=100:
+#     print(i)
+#     i+=1
+
+# i=1
+# while i<=10:
+#     print(3*i)
+#     i+=1
+
+# N=int(input("enter your positive integer(N)"))
+# total_sum=0 
+# i=1
+# while i <= N:
+#     total_sum += i
+#     i+=1
+
+# def fact(n):
+#     if(n==0 or n == 1):
+#       return 1
+#     else:
+#         return n*fact(n-1)
+# print(fact(5))
+
+# def count_even_odd(N):
+#    even_count = 0
+#    odd_count = 0
+#    for num in range (1,N+1):
+#       if num % 2 == 0:
+#          event_count += 1
+#       else:
+#        odd_count +=1
+#    return even_count,odd_count
+#                                   ###    19
+# str="madam"
+# if str==str[::-1]:
+#     print("string is palindrome:",str)
+# else:
+#     ("string is not palindrome:",str)
+
+# str= " hello gourav"
+# print(str.count("o"))
+# print(len(str))
+
+# text="abcdefghijklmnopqrstuvwxyz"
+# print(text[::-1])
+
