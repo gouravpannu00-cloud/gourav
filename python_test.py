3c39bae9d5b29a7a0a69e6758b2a9accfefad704
gouravpannu00-cloud <gouravpannu00@gmail.com>
2026-08-05 20:43:34 +0530

    Add files via upload

---
+++ python_test.py
@@ -0,0 +1,203 @@
+# value_2
+# float
+# %
+# input()
+# if
+# for loop
+# def
+# "a"
+# try-except
+# <class 'int'>
+
+
+# length=float(input("enter length:"))
+# breadth=float(input("enter breadth:"))
+# area = length * breadth
+# perimeter = 2 * (length + breadth)
+# print("area of rectengle=",area)
+# print ("perimeter of rectengle=",perimeter)
+
+
+
+
+# num= int(input("enter a number:"))
+# if num>0 :
+#     print("positive")
+# elif num == 0 :
+#     print("Zero")
+# else :
+#     num<0 
+#     print("negative")
+
+
+
+# i=2
+# while i <= 50 :
+#     print(i)
+#     i+=2
+
+
+
+# def is_prime(num):
+#     if num  <= 1 :
+#         return False
+#     for i in range (2,num):
+#         num % 2 == 0
+#         return False
+#     return True
+
+
+# num= int(input("enter a number:"))
+
+# print(is_prime(num))
+
+
+
+
+
+# try:
+#     num=float(input("enter first number : "))
+#     deno=float(input("enter second number:"))
+
+#     result= num / deno
+#     print(result)
+
+# except :
+#     print("error")
+
+
+
+
+
+# for i in range(1,6):
+#     for j in range (i):
+#         print ("*" , end=" ")
+#     print()
+
+
+
+
+# rows=5
+# for i in range(1, 6):
+#     for j in range (1 ,i+1):
+#         print (j,end = " ")
+#     print()
+
+
+
+
+# file = open("student.txt","w")
+
+# file.write("name: gourav\n")
+# file.write("name: pannu\n")
+# file.write("name: vansh\n")
+
+# file.close()
+
+
+
+
+
+# try:
+#     num1=int(input("enter 1st num:"))
+#     num2=int(input("enter 2nd num:"))
+#     num3=int(input("enter 3rd num:"))
+#     num4=int(input("enter 4th num:"))
+#     num5=int(input("enter 5th num:"))
+
+#     avg=(num1+num2+num3+num4+num5)/5
+#     print(avg)
+
+# except :
+#     print("Enter a valid number :")
+
+# finally :
+#     print ("program is complete")
+
+# name = "Gourav"
+# print(name)
+
+# age= 19
+# print(age)
+
+# num1=10
+# num2=20
+# print("sum =",num1+num2)
+
+# a=10
+# b=20
+# a,b=b,a
+# print("a=",a , "b=",b)
+
+# x="hello"
+# y=10
+# z=10.1
+# w=False
+# print(type(x))
+# print(type(y))
+# print(type(z))
+# print(type(w))
+
+# str_num= "25"
+# int_num =int(str_num)
+# print(int_num)
+
+# num=100
+# str_num=str(num)
+# print (str_num)
+
+# height=5.8
+# print(height)
+
+# name="Gourav"
+# age=19
+# city="Rohtak"
+# print("My name is:",name,"I am",age,"years old and I live in",city)
+
+# lenght=float(input("enter length:"))
+# breadth=float(input("enter breadth:"))
+# area= lenght*breadth
+# print("area of rectangle=",area)
+
+# radius=float(input("enter radius:"))
+# area=3.14*radius
+# area=("area of circle=",area)
+
+# p=float(input("enter principal:"))
+# r=float(input("enter rate :"))
+# t=float(input("enter Time :"))
+# si=(p*r*t)/100
+# print("simple interest=",si)
+
+# a=10
+# b=30
+# c=26
+# avg=(a+b+c)/3
+# print("average of three numbers=",avg)
+
+# num2=int(input("enter a number:"))
+# print("square of number=",num2**2)
+# print("cube of number=",num2**3)
+
+# num3=int(input("enter a number:"))
+# if num3 % 2 == 0:
+#     print("even number")
+# else:
+#     print("odd number")
+
+# units=int(input("enter units consumed:"))
+# bill = units * 5
+# print("total bill=",bill)
+
+# celsius=float(input("enter temperature in celsius:"))
+# fahrenheit=(celsius*9/5)+32
+# print("temperature in fahrenheit=",fahrenheit)
+
+# km=float(input("enter distance in km:"))
+# meters = km * 1000
+# print("distance in meters=",meters)
+
+# a= int(input("enter dividend:"))
+# b= int(input("enter divisor:"))
+# remainder = a- (a // b) * b
+# print("remainder=",remainder)
