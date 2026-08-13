ae7ab0479f751c3a727a1375a9e6e78e78c54799
gouravpannu00-cloud <gouravpannu00@gmail.com>
2026-08-13 16:37:21 +0530

    Add files via upload

---
+++ OOPS.py
@@ -0,0 +1,56 @@
+class person:
+    def __init__(self,name,age = 18):
+        self.name = name
+        self.age = age
+
+s1 = person("gourav",25)
+s2 = person("jatin")
+print(s1.name, s1.age)
+print(s2.name, s2.age)
+
+
+class student:
+    pass
+
+
+class student:
+    pass
+s1=student()
+
+
+class car: 
+    pass
+car1 = car()
+car2 = car()
+car3 = car()
+
+
+class student:
+    pass
+s1 = student()
+s1.name = "Gourav"
+
+print(s1.name)
+
+class student :
+    pass
+s1 = student()
+s2 = student()
+
+s1.name = "gourav"
+s2.name = "jatin"
+
+print(s1.name)
+print(s2.name)
+
+class student:
+    pass
+s1 = student()
+s1.name = "gourav"
+s1.age = 19
+s1.marks = 95
+print(s1.name)
+print(s1.age)
+print(s1.marks)
+
+
